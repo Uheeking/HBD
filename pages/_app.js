73ec289f82d4ea7b91fa2ef319629bbc8aa8dbf2
Uheeking <wert1847@naver.com>
2023-04-17 12:31:35 +0900

    ADD : head 추가 및 폰트 적용

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,31 @@
 import "@/styles/globals.css";
 import Image from "next/image";
-import chococake from "@/public/chococake.jpg";
+import chococake from "@/public/choco.png";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function App({ Component, pageProps }) {
   return (
-    <div style={{backgroundColor: "#B9A0FE", opacity: '0.8'}}>
+    <div>
       <Head>
-				<title>celebrate your birthday</title>
-        <link rel="metamong" href="/metamong.png" />
-				<meta name="description" content="HBD 페이지입니다." />
-			</Head>
+        <title>celebrate your birthday</title>
+        <link rel="icon" href="/metamong.png" />
+        <meta name="description" content="HBD 페이지입니다." />
+      </Head>
+      <Link href="/">
       <Image
+        className="pt-3"
         src={chococake}
         width={100}
         height={100}
         style={{ margin: "0 auto" }}
       />
+      </Link>
       {/* 갖고 싶은 선물 목록 선물 링크 선물 이미지 등*/}
-      <div>
-        <div>gift</div>
-        <div>card</div>
-        <div>photo</div>
+      <div className="flex p-5 headname">
+        <Link href="/">gift</Link>
+        <Link href="/card">card</Link>
+        <Link href="/photo">photo</Link>
       </div>
       <Component {...pageProps} />
     </div>
