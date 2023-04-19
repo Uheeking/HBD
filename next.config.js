e07e93fba527ec858c44c4dd89de9668b39a003d
Uheeking <wert1847@naver.com>
2023-04-19 14:34:55 +0900

    EDIT : photo page edit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
+const path = require('path')
 const nextConfig = {
   reactStrictMode: true,
   webpack5: true,
   swcMinify: true,
   webpack: (config) => {
     config.resolve.fallback = { fs: false };
-
+    config.resolve.alias["@"] = path.join(__dirname, "public");
     return config;
   },
 };
--- pages/photo.js
+++ pages/photo.js
@@ -1,24 +1,27 @@
 import Image from "next/image";
 import React from "react";
-import season from "@/public/photo/photo.jpg";
-import season2 from "@/public/photo/photo2.jpg";
-import season3 from "@/public/photo/photo3.jpg";
-import season4 from "@/public/photo/photo4.jpg";
-import chunsik from "@/public/chunsik.png";
+import seasonImage1 from "@/photo/photo.jpg";
+import seasonImage2 from "@/photo/photo2.jpg";
+import seasonImage3 from "@/photo/photo3.jpg";
+import seasonImage4 from "@/photo/photo4.jpg";
+import chunsikImage from "@/chunsik.png";
 
-export default function photo() {
+const IMAGE_WIDTH = 180;
+const IMAGE_HEIGHT = 100;
+
+export default function Photo() {
   return (
     <div>
       <div className="grid grid-cols-2 md:grid-cols-2 mt-0 m-7 gap-8 xs:w-full">
-        <Image src={season} width={180} height={100} />
-        <Image src={season2} width={180} height={100} />
-        <Image src={season3} width={180} height={100} />
-        <Image src={season4} width={180} height={100} />
+      <Image src={seasonImage1} width={IMAGE_WIDTH} height={IMAGE_HEIGHT} />
+        <Image src={seasonImage2} width={IMAGE_WIDTH} height={IMAGE_HEIGHT} />
+        <Image src={seasonImage3} width={IMAGE_WIDTH} height={IMAGE_HEIGHT} />
+        <Image src={seasonImage4} width={IMAGE_WIDTH} height={IMAGE_HEIGHT} />
       </div>
       <div className="shine text-center text-xl bg-white">
         우리의 모든 순간이 빛나기를✨
       </div>
-      <Image className="sik" src={chunsik} width={180} height={100} />
+      <Image className="chunsik-image" src={chunsikImage} width={IMAGE_WIDTH} height={IMAGE_HEIGHT} />
     </div>
   );
 }
